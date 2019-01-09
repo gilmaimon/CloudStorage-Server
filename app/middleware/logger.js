9daf27fc28f96a573de4452d416756732a8316d5
Gil Maimon <gilm2121@gmail.com>
2019-01-10 00:50:08 +0200

    fixed logger file path

--- app/middleware/logger.js
+++ app/middleware/logger.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var morgan = require('morgan');
 var path = require('path');
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '../../access.log'), { flags: 'a' });
 module.exports = morgan('combined', { stream: accessLogStream });
