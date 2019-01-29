dd5433ffe48594bc7af652683d1b5e23ebf917bb
Gil Maimon <gilm2121@gmail.com>
2019-01-29 23:07:05 +0200

    reformating

--- app/app.js
+++ app/app.js
@@ -11,20 +11,22 @@ app.locals.config = require('./config');
 Middlwares.AttachMiddlewares(app);
 Routes.SetupRoutes(app);
 
-module.exports = {start: function(callback) {
-    // Connect to databae and run server
-    var dbHelper = require('./database-helpers/database');
-    dbHelper.initDatabaseConnection(app.locals.config.mongodb_url, function(err, db) {
-        if(!err) {
-            app.locals.users = new Users(db);
-            console.log("Server Listening on localhost:" + app.locals.config.port);
-            var server = app.listen(app.locals.config.port);
-            if(callback) callback(function() {
-                server.close();
-            });
-        } else {
-            console.log("Database init error");
-            if(callback) callback(null);
-        }
-    });
-}};
+module.exports = {
+    start: function(callback) {
+        // Connect to databae and run server
+        var dbHelper = require('./database-helpers/database');
+        dbHelper.initDatabaseConnection(app.locals.config.mongodb_url, function(err, db) {
+            if(!err) {
+                app.locals.users = new Users(db);
+                console.log("Server Listening on localhost:" + app.locals.config.port);
+                var server = app.listen(app.locals.config.port);
+                if(callback) callback(function() {
+                    server.close();
+                });
+            } else {
+                console.log("Database init error");
+                if(callback) callback(null);
+            }
+        });
+    }
+};
